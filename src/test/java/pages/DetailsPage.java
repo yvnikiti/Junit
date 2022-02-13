078ab6ada95d2184e4e7a57c1898d185edf98764
Ekaterina_Ulyanova <katerina-lisichka@yandex.ru>
2022-02-13 18:39:32 +0300

    Написан второй тест

---
+++ src/test/java/pages/DetailsPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
+public class DetailsPage {
+    @FindBy(css = "#wp-tabs-container div[data-attrid*='poslednyaya_versiya'] .LrzXr.kno-fv")
+    private WebElement lastVersion;
+
+    @FindBy(css = "#wp-tabs-container div[data-attrid*='licenziya'] .LrzXr.kno-fv")
+    private WebElement license;
+
+    @FindBy(css = "#wp-tabs-container h2[data-attrid*='title']")
+    private WebElement name;
+
+    public DetailsPage(WebDriver driver) {
+        initElements(driver, this);
+    }
+    public String getLastVersion(){
+       return lastVersion.getText();
+    }
+    public String getLicense() {
+        return license.getText();
+    }
+        public String getName(){
+            return name.getText();
+}
+
+    }
--- src/test/java/tests/googleTests.java
+++ src/test/java/tests/googleTests.java
@@ -3,11 +3,10 @@ package tests;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import pages.DetailsPage;
 import pages.SearchPage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,6 +15,7 @@ public class googleTests {
 
     private static WebDriver driver;
     private static SearchPage SearchPage;
+    private static DetailsPage detailsPage;
 
     @BeforeAll
     public static void init() {
@@ -24,6 +24,7 @@ public class googleTests {
         options.addArguments("start-maximized");
         driver = new ChromeDriver(options);
         SearchPage = new SearchPage(driver);
+        detailsPage = new DetailsPage(driver);
     }
     @Test
     public void test1() {
@@ -32,6 +33,14 @@ public class googleTests {
         SearchPage.search("selenuim");
         assertEquals(13, SearchPage.results.size());
 
+    }
+    @Test
+    public void test2() {
+        driver.get ("http://google.com");
+        SearchPage.search("selenuim");
+        assertEquals("3.141.59 (14 ноября 2018 года)", detailsPage.getLastVersion());
+        assertEquals("Apache License 2.0", detailsPage.getLicense());
+        assertEquals("Selenium", detailsPage.getName());
     }
 @AfterAll
     public static void teardown() {
