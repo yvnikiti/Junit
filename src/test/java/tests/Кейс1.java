07b4a503eab8015c532bf23a0fa6cb9c48ec9156
Ekaterina_Ulyanova <katerina-lisichka@yandex.ru>
2022-02-13 20:07:18 +0300

    Частично написан первый кейс.

---
+++ src/test/java/tests/Кейс1.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import pages.DetailsPage;
+import pages.SearchPage;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Кейс1 {
+
+    private static WebDriver driver;
+    private static SearchPage SearchPage;
+    private static DetailsPage detailsPage;
+
+    @BeforeAll
+    public static void init() {
+        System.setProperty ("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+        SearchPage = new SearchPage(driver);
+        detailsPage = new DetailsPage(driver);
+    }
+    @BeforeEach
+    public void setup() {
+        driver.get("http://google.com");
+    }
+    @Test
+    @DisplayName("Проверка операций с целыми числами")
+    public void test1() {
+        SearchPage.search("Калькулятор");
+    }
+
+     @AfterAll
+    public static void teardown() {
+        driver.quit();
+    }
+}
