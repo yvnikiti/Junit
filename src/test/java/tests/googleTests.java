21b999563483ec874fb0f8116b9d2f0be69913b8
Ekaterina_Ulyanova <katerina-lisichka@yandex.ru>
2022-02-13 19:06:20 +0300

    Переписан второй тест

--- src/test/java/tests/googleTests.java
+++ src/test/java/tests/googleTests.java
@@ -1,14 +1,13 @@
 package tests;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import pages.DetailsPage;
 import pages.SearchPage;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class googleTests {
@@ -26,21 +25,26 @@ public class googleTests {
         SearchPage = new SearchPage(driver);
         detailsPage = new DetailsPage(driver);
     }
+    @BeforeEach
+    public void setup() {
+        driver.get("http://google.com");
+    }
     @Test
+    @DisplayName("Поиск числа результатов")
     public void test1() {
-
-        driver.get ("http://google.com");
         SearchPage.search("selenuim");
         assertEquals(13, SearchPage.results.size());
 
     }
     @Test
+    @DisplayName("Проверка деталей ресурса")
     public void test2() {
-        driver.get ("http://google.com");
         SearchPage.search("selenuim");
-        assertEquals("3.141.59 (14 ноября 2018 года)", detailsPage.getLastVersion());
-        assertEquals("Apache License 2.0", detailsPage.getLicense());
-        assertEquals("Selenium", detailsPage.getName());
+        assertAll(
+        () -> assertEquals("3.141.59 (14 ноября 2018 года)", detailsPage.getLastVersion()),
+        () -> assertEquals("Apache License 2.0", detailsPage.getLicense()),
+        () -> assertEquals("Selenium", detailsPage.getName())
+        );
     }
 @AfterAll
     public static void teardown() {
